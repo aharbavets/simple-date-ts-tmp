c89d7264e124581bc5d26e31d49f1e5e88358173
Alexander Gorbovets <alexander.gorbovets@gmail.com>
2023-02-10 14:43:43 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,277 @@
+export class SimpleDate {
+
+    private readonly raw: string
+
+    private readonly timezoneOffset: number
+
+    constructor(value: string|SimpleDate|Date, timezoneOffset?: number) {
+        this.timezoneOffset = timezoneOffset || (new Date().getTimezoneOffset())
+
+        this.raw = '0000-01-01'
+
+        switch (typeof value) {
+            case 'string': {
+                const matches = /^(\d+)-(\d+)-(\d+)/.exec(value)
+
+                if (!matches) {
+                    // we can't parse it, falling back to default JS parser
+                    throw new Error('')
+                }
+
+                this.raw = matches[0]
+
+                break
+            }
+
+            case "object":
+                if (value instanceof SimpleDate) {
+                    this.raw = value.getRaw()
+                }
+
+                if (value instanceof Date) {
+                    this.raw = value.toISOString().split('T')[0]
+                }
+
+                break
+        }
+    }
+
+    static create = (year: number, month: number, day: number) => {
+        year = Math.round(year)
+        month = Math.round(month)
+        day = Math.round(day)
+
+        if (month > 12) {
+            month = 12
+        }
+
+        if (month < 1) {
+            month = 1
+        }
+
+        if (day < 0) {
+            day = 1
+        }
+
+        const countOfDaysInMonth = SimpleDate.daysInMonth(month, year)
+        if (day > countOfDaysInMonth) {
+            day = countOfDaysInMonth
+        }
+
+        const padStart = (value: number, count: number, padCharacter: string = '0') => {
+            let result = String(value)
+            while (result.length < count) {
+                result = padCharacter + result
+            }
+            return result
+        }
+
+        const y = padStart(year, 4, '0')
+        const m = padStart(month, 2, '0')
+        const d = padStart(day, 2, '0')
+
+        return new SimpleDate(`${y}-${m}-${d}`)
+    }
+
+    static zero = () => new SimpleDate('0000-01-01')
+
+    equals = (other: SimpleDate) => this.raw === other.getRaw()
+
+    lessThan = (other: SimpleDate) => this.raw < other.getRaw()
+
+    lessThanOrEqual = (other: SimpleDate) => this.raw <= other.getRaw()
+
+    greaterThan = (other: SimpleDate) => this.raw > other.getRaw()
+
+    greaterThanOrEqual = (other: SimpleDate) => this.raw >= other.getRaw()
+
+    toString = () => this.raw
+
+    toJsDate = () => {
+        const sign = this.timezoneOffset > 0 ? '+' : '-'
+        const timeZoneOffset = this.timezoneOffset / 60 + ':' + this.timezoneOffset % 60
+        return new Date(this.raw + 'T00:00:00.000' + sign + timeZoneOffset)
+    }
+
+    toJsDateInUTC = () => new Date(this.raw + 'T00:00:00.000' + 'Z');
+
+    toTimestamp = () => this.toJsDateInUTC().getTime()
+
+    getRaw = () => this.raw
+
+    getYear = (): number => Number(this.raw.split('-')[0])
+
+    getMonth = (): number => Number(this.raw.split('-')[1])
+
+    getDay = (): number => Number(this.raw.split('-')[2])
+
+    getFullMonth = () => {
+        const [year, month] = this.raw.split('-')
+        return `${year}-${month}`
+    }
+
+    getFullSemester = () => {
+        const [year, rawMonth] = this.raw.split('-')
+
+        const month = parseInt(rawMonth, 10)
+
+        if (month >= 1 && month <= 6) {
+            return `${year}-01 — ${year}-06`
+        }
+        if (month >= 7 && month <= 12) {
+            return `${year}-07 — ${year}-12`
+        }
+
+        return ''
+    }
+
+    getFullQuarter = () => {
+        const [year, rawMonth] = this.raw.split('-')
+
+        const month = parseInt(rawMonth, 10)
+
+        if (month >= 1 && month <= 3) {
+            return `${year}-01 — ${year}-03`
+        }
+        if (month >= 4 && month <= 6) {
+            return `${year}-04 — ${year}-06`
+        }
+        if (month >= 7 && month <= 9) {
+            return `${year}-07 — ${year}-09`
+        }
+        if (month >= 10 && month <= 12) {
+            return `${year}-10 — ${year}-12`
+        }
+
+        return ''
+    }
+
+    addDays(count: number) {
+        count = Math.round(count)
+
+        if (count > 0) {
+            return this._addDays(count)
+        } else if (count < 0) {
+            return this._subtractDays(Math.abs(count))
+        } else {
+            return new SimpleDate(this.raw)
+        }
+    }
+
+    protected _addDays = (count: number) => {
+        let year = this.getYear()
+        let month = this.getMonth()
+        let day = this.getDay()
+
+        while (count-- > 0) {
+            day++
+            if (day > SimpleDate.daysInMonth(month, year)) {
+                day = 1
+                month++
+
+                if (month > 12) {
+                    month = 1
+                    year++
+                }
+            }
+        }
+
+        return SimpleDate.create(year, month, day)
+    }
+
+    protected _subtractDays(count: number) {
+        let year = this.getYear()
+        let month = this.getMonth()
+        let day = this.getDay()
+
+        while (count-- > 0) {
+            day--
+            if (day < 1) {
+                month--
+
+                if (month < 1) {
+                    month = 12
+                    year--
+                }
+
+                day = SimpleDate.daysInMonth(month, year)
+            }
+
+        }
+
+        return SimpleDate.create(year, month, day)
+    }
+
+    protected static daysInMonth(month: number, year: number) {
+        switch (month) {
+            case 1: return 31
+            case 2: return year % 4 === 0 ? 29 : 28
+            case 3: return 31
+            case 4: return 30
+            case 5: return 31
+            case 6: return 30
+            case 7: return 31
+            case 8: return 31
+            case 9: return 30
+            case 10: return 31
+            case 11: return 30
+            case 12: return 31
+            default: return 0
+        }
+    }
+
+    addMonths(count: number) {
+        count = Math.round(count)
+
+        if (count > 0) {
+            return this._addMonths(count)
+        } else if (count < 0) {
+            return this._subtractMonths(Math.abs(count))
+        } else {
+            return new SimpleDate(this.raw)
+        }
+    }
+
+    protected _addMonths = (count: number) => {
+        let year = this.getYear()
+        let month = this.getMonth()
+        const day = this.getDay()
+
+        while (count-- > 0) {
+            month++
+
+            if (month > 12) {
+                month = 1
+                year++
+            }
+        }
+
+        return SimpleDate.create(year, month, day)
+    }
+
+    protected _subtractMonths(count: number) {
+        let year = this.getYear()
+        let month = this.getMonth()
+        const day = this.getDay()
+
+        while (count-- > 0) {
+            month--
+
+            if (month < 1) {
+                month = 12
+                year--
+            }
+        }
+
+        return SimpleDate.create(year, month, day)
+    }
+
+    addYears(count: number) {
+        const year = this.getYear()
+        const month = this.getMonth()
+        const day = this.getDay()
+
+        return SimpleDate.create(year + count, month, day)
+    }
+
+}
